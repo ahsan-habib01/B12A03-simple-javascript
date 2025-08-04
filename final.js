4bb43b72613f92104ecbe5554676e757a80a6f52
ahsan.habib01 <ahshapon00@gmail.com>
2025-08-04 10:32:50 +0600

    complete problem 1 and problem 2

---
+++ final.js
@@ -0,0 +1,18 @@
+/** Problem -01 ( Divide the Asset ) */
+var area = 800;
+//write your code here
+
+console.log(area / 2);
+
+
+/** Problem -02 ( Cycle or Laptop ) */
+var money = 29999;
+//write your code here
+
+if (money >= 25000) {
+  console.log('Laptop');
+} else if (money >= 10000) {
+  console.log('Cycle');
+} else {
+  console.log('Chocolate');
+}
