a61df73e4c6e897cb26334b0920f12edbb66b817
ahsan.habib01 <ahshapon00@gmail.com>
2025-08-04 10:34:29 +0600

    complete problem 3

--- final.js
+++ final.js
@@ -16,3 +16,16 @@ if (money >= 25000) {
 } else {
   console.log('Chocolate');
 }
+
+
+/** Problem -03 ( Medicine Planner ) */
+var lastDay = 11;
+//write your code here
+
+for (var i = 1; i <= lastDay; i++) {
+  if (i % 3 === 0) {
+    console.log(i, '- medicine');
+  } else {
+    console.log(i, '- rest');
+  }
+}
